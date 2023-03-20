32f9303f59f10c16c7a167af1f67c42bb1dbe686
lowie2727 <76224096+lowie2727@users.noreply.github.com>
2023-03-20 16:11:45 +0100

    files are generated based on the current time

---
+++ arduino/main/README.md
@@ -0,0 +1,11 @@
+# Main
+
+Datafiles are generated in the following folder structure: YYYY/MM/DDTHH-MM.csv (the leading 0 is not shown yet)
+
+## Notes
+
+When pressing the reset button the clockModule does not update the time
+
+## TODO
+
+Adding stop and start button
--- arduino/main/main.ino
+++ arduino/main/main.ino
@@ -1,7 +1,83 @@
+// clock module
+#include "GravityRtc.h"
+#include "Wire.h"
+
+GravityRtc rtc;  //RTC Initialization
+
+// sd card
+#include <SPI.h>
+#include <SD.h>
+
+const int chipSelect = 4;
+const String headers = "column1;column2;column3";
+String filePath;
+
 void setup() {
-  // put your setup code here, to run once:
+  Serial.begin(9600);
+  Serial.println();
+
+  sdCardInit();
+  clockModuleSetup();
+  updateFileName();
+  writeHeaders();
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  for (int i = 0; i < 10; i++) {
+    int random1 = random(20);
+    int random2 = random(20);
+    int random3 = random(20);
+    String data = String(random1) + ";" + String(random2) + ";" + String(random3);
+    stringToSd(data);
+    delay(1000);
+  }
+}
+
+void sdCardInit() {
+  Serial.print("Initializing SD card...");
+
+  // see if the card is present and can be initialized:
+  if (!SD.begin(chipSelect)) {
+    Serial.println("Card failed, or not present");
+    while (1) {};
+  }
+  Serial.println("card initialized.");
+}
+
+void clockModuleSetup() {
+  rtc.setup();
+  rtc.adjustRtc(F(__DATE__), F(__TIME__));
+}
+
+void updateFileName() {
+  rtc.read();
+  String directory = String(rtc.year) + "/" + String(rtc.month) + "/";
+  String fileName = String(rtc.day) + "T" + String(rtc.hour) + "-" + String(rtc.minute) + ".csv";
+
+  SD.mkdir(directory);
+
+  filePath = directory + fileName;
+  Serial.println(filePath);
+
+  File dataFile = SD.open(filePath, FILE_WRITE);
+
+  if (dataFile) {
+    dataFile.close();
+  } else {
+    Serial.println("error opening " + filePath);
+  }
+}
+
+void stringToSd(String data) {
+  File dataFile = SD.open(filePath, FILE_WRITE);
+  if (dataFile) {
+    dataFile.println(data);
+    dataFile.close();
+  } else {
+    Serial.println("error opening " + filePath);
+  }
+}
+
+void writeHeaders() {
+  stringToSd(headers);
 }
