5c09ceca94cd5ce751177d3677881acbc3b3c779
lowie2727 <76224096+lowie2727@users.noreply.github.com>
2023-06-20 23:50:20 +0200

    LCD fix

--- arduino/src/components/LCD/LCD.cpp
+++ arduino/src/components/LCD/LCD.cpp
@@ -12,11 +12,11 @@
   Written by Limor Fried/Ladyada for Adafruit Industries.
   MIT license, all text above must be included in any redistribution
  ****************************************************/
+#include <Arduino.h>
 
-
-#include "SPI.h"
 #include "Adafruit_GFX.h"
 #include "Adafruit_ILI9341.h"
+#include "SPI.h"
 
 // For the Adafruit shield, these are the default.
 #define TFT_DC 7
@@ -27,19 +27,20 @@
 #define TFT_MISO 50
 
 // Use hardware SPI (on Uno, #13, #12, #11) and the above for CS/DC
-//Adafruit_ILI9341 tft = Adafruit_ILI9341(TFT_CS, TFT_DC);
+// Adafruit_ILI9341 tft = Adafruit_ILI9341(TFT_CS, TFT_DC);
 // If using the breakout, change pins as desired
-Adafruit_ILI9341 tft = Adafruit_ILI9341(TFT_CS, TFT_DC, TFT_MOSI, TFT_CLK, TFT_RST, TFT_MISO);
+Adafruit_ILI9341 tft =
+    Adafruit_ILI9341(TFT_CS, TFT_DC, TFT_MOSI, TFT_CLK, TFT_RST, TFT_MISO);
+
+unsigned long testText();
 
 void setup() {
   Serial.begin(9600);
-  Serial.println("ILI9341 Test!"); 
- 
+  Serial.println("ILI9341 Test!");
+
   tft.begin();
   tft.fillScreen(ILI9341_BLACK);
-
 }
-
 
 void loop(void) {
   testText();
@@ -50,11 +51,14 @@ unsigned long testText() {
   tft.fillScreen(ILI9341_BLACK);
   unsigned long start = micros();
   tft.setCursor(0, 0);
-  tft.setTextColor(ILI9341_WHITE);  tft.setTextSize(1);
+  tft.setTextColor(ILI9341_WHITE);
+  tft.setTextSize(1);
   tft.println("Hello World!");
-  tft.setTextColor(ILI9341_YELLOW); tft.setTextSize(2);
+  tft.setTextColor(ILI9341_YELLOW);
+  tft.setTextSize(2);
   tft.println(1234.56);
-  tft.setTextColor(ILI9341_RED);    tft.setTextSize(3);
+  tft.setTextColor(ILI9341_RED);
+  tft.setTextSize(3);
   tft.println(0xDEADBEEF, HEX);
   tft.println();
   tft.setTextColor(ILI9341_GREEN);
